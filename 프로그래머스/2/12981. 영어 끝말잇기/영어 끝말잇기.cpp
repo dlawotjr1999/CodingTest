0f3bb7452088654ed1ef647086688c5945b19fdc
임재석 <43702356+dlawotjr1999@users.noreply.github.com>
2026-02-23 16:23:52 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -10,19 +10,14 @@ vector<int> solution(int n, vector<string> words) {
     unordered_map<string, bool> visited;
 
     visited[words[0]] = true;
-    int turn = 1, cur = 1;
     
     for(int i = 1; i < words.size(); ++i) {
         if(visited[words[i]] || words[i - 1].back() != words[i][0]) {
-            answer = {cur + 1, turn};
+            answer = {(i % n) + 1, i/n + 1};
             break;
         }
         
         visited[words[i]] = true;
-        cur++;
-        if(cur == n) {
-            turn++; cur = 0;
-        }
     }    
     
     return answer;
