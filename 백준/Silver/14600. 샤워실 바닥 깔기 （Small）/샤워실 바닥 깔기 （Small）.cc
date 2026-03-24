67b0aaa9fd38e8a7648e086c5f66d63d5d113d87
임재석 <43702356+dlawotjr1999@users.noreply.github.com>
2026-03-24 10:35:08 +0900

    [Silver I] Title: 샤워실 바닥 깔기 (Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14600. 샤워실 바닥 깔기 （Small）/샤워실 바닥 깔기 （Small）.cc
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+const int dir_x[4][3] = { {0,0,-1},{0,0,1},{0,-1,0},{0,1,0} };
+const int dir_y[4][3] = { {0,-1,0},{0,-1,0},{0,0,1},{0,0,1} };
+
+int field[4][4];
+
+int K;
+int x, y;
+
+int N;
+bool isCompleted;
+
+void DFS(int num, int cnt) {
+	if (isCompleted)
+		return;
+
+	if (cnt == (N * N) - 1) {
+		for (int i = (N - 1); i >= 0; --i) {
+			for (int j = 0; j < N; ++j) {
+				std::cout << field[i][j] << ' ';
+			}
+			std::cout << '\n';
+		}
+
+		isCompleted = true;
+		return;
+	}
+
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			if (field[i][j] == 0) {
+
+				for (int d = 0; d < 4; ++d) {
+					bool isPossible = true;
+
+					for (int e = 0; e < 3; ++e) {
+						int dx = j + dir_x[d][e];
+						int dy = i + dir_y[d][e];
+
+						if (dx < 0 || dx >= N || dy < 0 || dy >= N || field[dy][dx] != 0) {
+							isPossible = false;
+							break;
+						}
+					}
+
+					if (isPossible) {
+						for (int e = 0; e < 3; ++e) {
+							int dx = j + dir_x[d][e];
+							int dy = i + dir_y[d][e];
+							
+							field[dy][dx] = num;
+							cnt++;
+						}
+
+						DFS(num + 1, cnt);
+
+						for (int e = 0; e < 3; ++e) {
+							int dx = j + dir_x[d][e];
+							int dy = i + dir_y[d][e];
+
+							field[dy][dx] = 0;
+							cnt--;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	std::cin >> K;
+	std::cin >> x >> y;
+	N = 1 << K;
+
+	field[y - 1][x - 1] = -1;
+	DFS(1, 0);
+
+	return 0;
+}
