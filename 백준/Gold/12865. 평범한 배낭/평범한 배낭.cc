4bca7eaf89c3f6f2174877b01e84d39f29c20af3
임재석 <43702356+dlawotjr1999@users.noreply.github.com>
2026-04-02 21:03:36 +0900

    [Gold V] Title: 평범한 배낭, Time: 40 ms, Memory: 42004 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	int N, K;
+	std::vector<std::pair<int, int>> p;
+
+	std::cin >> N >> K;
+	std::vector<vector<int>> dp(N + 1, std::vector<int>(K + 1));
+
+	p.emplace_back(0, 0);
+	for (int i = 1; i < N + 1; i++) {
+		int w, v;
+		std::cin >> w >> v;
+
+		p.emplace_back(w, v);
+	}
+
+	for (int i = 1; i < dp.size(); i++) {
+		for (int j = 1; j < dp[i].size(); j++) {
+			if (p[i].first > j)
+				dp[i][j] = dp[i - 1][j];
+			else
+				dp[i][j] = std::max(dp[i - 1][j], dp[i - 1][j - p[i].first] + p[i].second);
+		}
+	}
+
+	std::cout << dp[N][K] << std::endl;
+
+	return 0;
+}
