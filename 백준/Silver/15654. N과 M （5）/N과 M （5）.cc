06303d8603aab3e010ac0bd920919676bc8270a7
임재석 <43702356+dlawotjr1999@users.noreply.github.com>
2026-04-09 21:50:27 +0900

    [Silver III] Title: N과 M (5), Time: 32 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -1,37 +1,35 @@
 #include <bits/stdc++.h>
 
-void DFS(std::vector<int>& vec, std::vector<bool>& visited, std::vector<int>& cur, int M) {
-	if (cur.size() == M) {
-		for (int elm : cur)
-			std::cout << elm << ' ';
+int N, M;
+std::vector<int> path;
+std::vector<int> vec;
+
+void DFS(int depth, int mask) {
+	if (depth == M) {
+		for (int x : path) {
+			std::cout << x << ' ';
+		}
 		std::cout << '\n';
 		return;
 	}
-
-	for (int i = 0; i < vec.size(); ++i) {
-		if (!visited[i]) {
-			visited[i] = true;
-			cur.push_back(vec[i]);
-			DFS(vec, visited, cur, M);
-			cur.pop_back();
-			visited[i] = false;
-		}
+	
+	for (int i = 0; i < N; ++i) {
+		if (mask & (1 << i))
+			continue;
+		path.push_back(vec[i]);
+		DFS(depth + 1, mask | (1 << i));
+		path.pop_back();
 	}
 }
 
 int main() {
-	int N, M;
 	std::cin >> N >> M;
+	vec.resize(N);
 
-	std::vector<int> vec(N, 0);
 	for (int i = 0; i < N; ++i) {
 		std::cin >> vec[i];
 	}
 	std::sort(vec.begin(), vec.end());
 
-	std::vector<int> cur;
-	std::vector<bool> visited(N, false);
-	DFS(vec, visited, cur, M);
-
-	return 0;
+	DFS(0, 0);
 }
