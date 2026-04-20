bf0534485e02523703d748f55b2264f8c692d47c
임재석 <43702356+dlawotjr1999@users.noreply.github.com>
2026-04-20 11:29:36 +0900

    [level 2] Title: 디펜스 게임, Time: 24.03 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(int n, int k, vector<int> enemy) {
+    priority_queue<int> pq;
+    
+    for(int i = 0; i < (int)enemy.size(); ++i) {
+        n -= enemy[i];
+        pq.push(enemy[i]);
+        
+        if(n < 0) {
+            if(k == 0)
+                return i;
+            
+            n += pq.top();
+            pq.pop();  
+            k--;
+        }
+    }
+    
+    return enemy.size();
+}
